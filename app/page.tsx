b791d1114a9ee260789127832c349d1f4af2fa78
Bright Esosa Ogiesoba <157101723+BAREO14@users.noreply.github.com>
2024-02-08 19:03:40 +0000

    ✔Gratitude
    🔰

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (<>
+  <h1>I am <b>Bright Esosa Ogiesoba</b></h1>
+  <p>I am a <mark>Programmer</mark> who loves Programming</p>
+  <p>I love drinking H<sub>2</sub>O and there is usually a bottle of H<sub>2</sub>O on my desk anytime i'm coing.  
+  O<sup>2</sup> is also very compulsory in the life of any programmer</p>
+  <h2>Apart from <u>coding</u>, there are other things I
+    enjoy doing and they <i>include</i> <small>writing songs</small>,
+    frestyling and spending time with fellow <mark>Programmers</mark> that helps me do away with unnecessary<s>distractions</s> and helps me Focus more</h2>
+  <p>. </p>
+  </>
+
+  );
+}

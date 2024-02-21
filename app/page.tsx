e0a60b1d1f6bab92dbf33f7d2263d0eab57ad156
Bright Esosa Ogiesoba <157101723+BAREO14@users.noreply.github.com>
2024-02-21 09:20:23 +0000

    More on html Tags✅

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,11 @@ export default function Home() {
   <p>I am a <mark>Programmer</mark> who loves Programming</p>
   <p>I love drinking H<sub>2</sub>O and there is usually a bottle of H<sub>2</sub>O on my desk anytime i&apos;m coding.  O<sup>2</sup> is also very compulsory in the life of any programmer</p>
   <h2>Apart from <u>coding</u>, there are other things I enjoy doing and they <i>include</i> <small>writing songs</small> frestyling and spending time with fellow <mark>Programmers</mark> that helps me do away with unnecessary <s>distractions</s> and helps me Focus more on important tasks</h2>
+<p>Print this page by pressing <kbd> ctrl + p </kbd> on your keyboard</p>
+<p>Bright loves coding in <pre><code>html, css, javascript, next.js, react</code></pre></p>
+<p>I really really so  much hate Tinubu of <abbr title='All Progressive congress'> APC</abbr></p>
+<p>I live at <address>No.16 Carnival street, Off Upper County, Sapele. </address></p>
+<p><bdo dir='rtl'>This is a right to left and can be left to right</bdo></p>
   </>
   )
 }
